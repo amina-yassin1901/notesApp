6004f76b70dfd5db54641ca4e2d5fb5719ddf774
AminaYassin <amina6012016@gmail.com>
2026-03-11 09:57:24 +0100

    init

---
+++ src/components/header/index.jsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.css";
+function Header() {
+  return <h1 className={styles.header}>My Notes App</h1>;
+}
+export default Header;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+const initialState = {
+  todos: [],
+};
+
+const todoReducer = (state = initialState, action) => {
+  return state;
+};
+export default todoReducer;
